a1fb57d6285af5a35d8b3cfd1a7c09ececb93a12
Dev Zero <litkaponii@gmail.com>
2026-05-24 14:18:35 -0500

    feat: agregar DoctorRepository

--- api-hospital-springboot/src/main/java/com/example/demo/repository/DoctorRepository.java
+++ api-hospital-springboot/src/main/java/com/example/demo/repository/DoctorRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.model.Doctor;
 
-public interface DoctorRepository {
+public interface DoctorRepository extends JpaRepository<Doctor, Long>{
 
 }
