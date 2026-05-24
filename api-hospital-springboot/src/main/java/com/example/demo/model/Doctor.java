0f8bea9ed9dff5930dcd38cb9170e641270ca304
Dev Zero <litkaponii@gmail.com>
2026-05-24 14:10:13 -0500

    feat: agregar clase Doctor

---
+++ api-hospital-springboot/src/main/java/com/example/demo/model/Doctor.java
@@ -0,0 +1,103 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "doctor")
+public class Doctor {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nombre",
+			nullable = false)
+	private String nombre;
+	
+	@Column(name = "especialidad",
+			nullable = false)
+	private String especialidad;
+	
+	@Column(name = "correo",
+			nullable = false)
+	private String correo;
+	
+	@Column(name = "telefono",
+			nullable = false)
+	private String telefono;
+	
+
+	public Doctor(Long id, String nombre, String especialidad, String correo, String telefono) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.especialidad = especialidad;
+		this.correo = correo;
+		this.telefono = telefono;
+	}
+
+
+	public Doctor() {
+		super();
+		
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
+
+	public String getCorreo() {
+		return correo;
+	}
+
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	
+	
+}
