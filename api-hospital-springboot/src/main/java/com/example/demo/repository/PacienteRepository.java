b1b0f9be5c42930e0dc5573a17b0a0281278099e
Dev Zero <litkaponii@gmail.com>
2026-05-25 21:12:17 -0500

    feat: agregar PasienteRepository

---
+++ api-hospital-springboot/src/main/java/com/example/demo/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Paciente;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long>{
+
+}
