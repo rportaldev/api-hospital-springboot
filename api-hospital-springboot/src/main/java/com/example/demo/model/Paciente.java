c2ea1a582fc18298eb5ab2f9f71abf1a6681e8f2
Dev Zero <litkaponii@gmail.com>
2026-05-25 21:08:58 -0500

    feat: agregar clase Paciente

---
+++ api-hospital-springboot/src/main/java/com/example/demo/model/Paciente.java
@@ -0,0 +1,117 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "paciente")
+public class Paciente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nombre",
+			nullable = false)
+	private String nombre;
+	
+	@Column(name = "apellido",
+			nullable = false)
+	private String apellido;
+	
+	@Column(name = "edad",
+			nullable = false)
+	private int edad;
+	
+	@Column(name = "correo",
+			nullable = false)
+	private String correo;
+	
+	@Column(name = "telefono",
+			nullable = false)
+	private String telefono;
+
+	
+	public Paciente(Long id, String nombre, String apellido, int edad, String correo, String telefono) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.correo = correo;
+		this.telefono = telefono;
+	}
+
+
+	public Paciente() {
+		super();
+		
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+
+	public String getCorreo() {
+		return correo;
+	}
+
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+
+	
+}
