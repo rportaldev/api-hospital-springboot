c6b57173afa85110ae7b782ea19e03b4948598ea
Dev Zero <litkaponii@gmail.com>
2026-05-27 21:49:02 -0500

    feat: proyecto API REST Hospital completado

--- api-hospital-springboot/src/main/java/com/example/demo/model/Doctor.java
+++ api-hospital-springboot/src/main/java/com/example/demo/model/Doctor.java
@@ -2,6 +2,8 @@ package com.example.demo.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -34,6 +36,7 @@ public class Doctor {
 			nullable = false)
 	private String telefono;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "doctor")
 	private List<Cita> citas;
 
