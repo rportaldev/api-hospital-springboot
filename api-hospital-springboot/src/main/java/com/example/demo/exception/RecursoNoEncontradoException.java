b5f3adf21b7581d88564e5499ec5e5fdb410a7fa
Dev Zero <litkaponii@gmail.com>
2026-08-25 21:12:58 -0500

    fix: actualizar RecursoNoEncontradoException

--- api-hospital-springboot/src/main/java/com/example/demo/exception/RecursoNoEncontradoException.java
+++ api-hospital-springboot/src/main/java/com/example/demo/exception/RecursoNoEncontradoException.java
@@ -3,7 +3,7 @@ package com.example.demo.exception;
 public class RecursoNoEncontradoException extends RuntimeException{
 
 	
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
     
     public RecursoNoEncontradoException(String mensaje) {
         super(mensaje);
